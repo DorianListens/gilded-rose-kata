72256b46e59bf200087e11ee3cf57b659d3e52dc
Dorian <dorian.scheidt@gmail.com>
2015-03-20 17:52:53 -0400

    Make template simplier, remove many unneccessary functions

--- inventory_item.rb
+++ inventory_item.rb
@@ -20,49 +20,24 @@ class InventoryManager
   end
 
   def update
-    update_sell_in
-    update_item_quality
-  end
-
-  def update_sell_in
     @item.sell_in -= 1
+    change_item_quality(amount)
   end
 
-  def update_item_quality
-    if should_reduce_item_quality
-      reduce_item_quality
-    else
-      increase_item_quality
+  def change_item_quality(amount)
+    @item.quality += amount
+    if @item.quality <= 0
+      @item.quality = 0
+    elsif @item.quality >= 50
+      @item.quality = 50
     end
   end
 
-  def should_reduce_item_quality
-    true
-  end
-
-  def reduce_item_quality
-    if @item.quality > 0
-      @item.quality -= amount_to_reduce_quality_by
-    end
-  end
-
-  def increase_item_quality
-    new_quality = @item.quality += amount_to_increase_quality_by
-    if new_quality > 50
-      new_quality = 50
-    end
-    @item.quality = new_quality
-  end
-
-  def amount_to_increase_quality_by
-    1
-  end
-  
-  def amount_to_reduce_quality_by
+  def amount
     if @item.sell_in > 0
-      1
+      -1
     else
-      2
+      -2
     end
   end
 end
@@ -73,42 +48,38 @@ class LegendaryInventoryManager < InventoryManager
 end
 
 class AgedInventoryManager < InventoryManager
-  def amount_to_increase_quality_by
+  def amount
     if @item.sell_in < 0
       2
     else
       1
     end
   end
-
-  def should_reduce_item_quality
-    false
-  end
 end
 
 class BackstageInventoryManager < InventoryManager
-  def amount_to_increase_quality_by
-    amount = 1
-    if @item.sell_in < 10
-      amount += 1
+  def amount
+    if is_expired
+      amount = - @item.quality
+    else
+      amount = 1
+      if @item.sell_in < 10
+        amount += 1
+      end
+      if @item.sell_in < 5
+        amount += 1
+      end
+      amount
     end
-    if @item.sell_in < 5
-      amount += 1
-    end
-    amount
   end
 
-  def should_reduce_item_quality
+  def is_expired
     @item.sell_in < 0
-  end
-
-  def amount_to_reduce_quality_by
-    @item.quality
   end
 end
 
 class ConjuredInventoryManager < InventoryManager
-  def amount_to_reduce_quality_by
+  def amount
     super * 2
   end
 end
